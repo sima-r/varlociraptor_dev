fb1938ba0b0f68e530546b2a49ca67a0ac3ace73
sima-r <sima.rahimi@skane.se>
2020-05-20 14:51:10 +0200

    put merging in a separate function

--- merge_for_varlociraptor.py
+++ merge_for_varlociraptor.py
@@ -4,86 +4,92 @@ import gzip
 from optparse import OptionParser
 
 
-Parser = OptionParser()
-Parser.add_option("--callers", action='store', dest='callers',help="A list of variant callers to be included.")
-Parser.add_option("--output", action='store', dest='output_name', help = "Name of outfile")
-Parser.add_option("--dir", action='store', dest='analysis_dirPath', help= "Tha PATH of the directory where vcf files locate.")
-#Parser.add_option('-r', "--resultLUID", action='store', dest='resultLUID')
 
-#(options, args) = Parser.parse_args()
-options=  Parser.parse_args()[0]
+def merge_vcfs(all_vcfs,analysis_dirPath,fout_name):
+    """
+    This  function (merge_vcfs) merges a list of vcfs for varlociraptor sw. 
+    From header lines only lines with contig,INFO and ALT and from columns 
+    CHROM, POS, ID, REF, ALT and INFO are written into outfile.
+    """
+    #print(merge_vcfs.__doc__)
+    print('Merging...')
+    header_lines=[]
+    lines= []
+    tags=[]
 
-callers= options.callers
-callers = callers.split(',')
-analysis_dirPath = options.analysis_dirPath
-fout_name = options.output_name
-
-print(analysis_dirPath, callers, fout_name)
-all_vcfs =[]
-for name in callers:
-    pattern = '*' + name + '.vcf.gz'
-    globals()[name] = fnmatch.filter(os.listdir(analysis_dirPath), pattern)
-    
-    files = fnmatch.filter(os.listdir(analysis_dirPath), pattern)
-    all_vcfs = all_vcfs + files
-
-#analysis_dirPath = '/fs1/results/myeloid/vcf/'
-#freebayes = fnmatch.filter(os.listdir(analysis_dirPath), '*freebayes.vcf.gz')
-#vardict = fnmatch.filter(os.listdir(analysis_dirPath), '*vardict.vcf.gz')
-#tnscope = fnmatch.filter(os.listdir(analysis_dirPath), '*tnscope.vcf.gz')
-#all_vcfs= freebayes +  vardict +  tnscope
-
-
-
-
-
-header_lines=[]
-lines= []
-tags=[]
-
-for  file in all_vcfs:
-    file_path = analysis_dirPath + file
-    print(file_path)
-    with gzip.open(file_path, 'rt') as fin, open(fout_name , 'w') as merged_file:
-        for line in fin:
-                line = line.rstrip()
-                #try to extract INFO/contig tag lines from headers.
-                if  line.startswith('##'):
-                    if line.startswith('##contig') or line.startswith('##INFO')  or  line.startswith('ALT'):
-                        if line.split('<')[1].split(',')[0] not in tags:
-                            tags.append(line.split('<')[1].split(',')[0])
-                            header_lines.append(line)
-                    
-                elif  line.startswith('#CHROM'):
-                        continue
-                #try to get file contents
-                else:
-                    cols=line.split('\t')
-                    new_line = '\t'.join(cols[0:5])
-                    #check if  'SVLEN' is present
-                    if  'SVLEN' in cols[7]:
-                        new_line = new_line + '\t' + cols[7]
+    for  file in all_vcfs:
+        file_path = analysis_dirPath + file
+        #print(file_path)
+        with gzip.open(file_path, 'rt') as fin, open(fout_name , 'w') as merged_file:
+            for line in fin:
+                    line = line.rstrip()
+                    #try to extract INFO/contig tag lines from headers.
+                    if  line.startswith('##'):
+                        if line.startswith('##contig') or line.startswith('##INFO')  or  line.startswith('ALT'):
+                            if line.split('<')[1].split(',')[0] not in tags:
+                                tags.append(line.split('<')[1].split(',')[0])
+                                header_lines.append(line)
+                        
+                    elif  line.startswith('#CHROM'):
+                            continue
+                    #try to get file contents
                     else:
-                        new_line =  new_line + '\t' + '.'
-                    if new_line not in lines:
-                        lines.append(new_line)
-        #collect all lines
-        all_lines = header_lines + ['#CHROM\tPOS\tID\tREF\tALT\tINFO'] +  lines
-        for line in all_lines:
-            print(line, file= merged_file) #print lines into output file
+                        cols=line.split('\t')
+                        new_line = '\t'.join(cols[0:5])
+                        #check if  'SVLEN' is present
+                        if  'SVLEN' in cols[7]:
+                            new_line = new_line + '\t' + cols[7]
+                        else:
+                            new_line =  new_line + '\t' + '.'
+                        if new_line not in lines:
+                            lines.append(new_line)
+            #collect all lines
+            all_lines = header_lines + ['#CHROM\tPOS\tID\tREF\tALT\tINFO'] +  lines
+            for line in all_lines:
+                print(line, file= merged_file) #print lines into output file
+    print("Merging vcf files has been done.\n#headers-->{}\n#lines-->{}".format(len(header_lines),len(all_lines)))        
 
-
-print('#header lines' , len(header_lines))
-print('#lines', len(lines))
-
-
-'''
-def main():
     
-    This script merges the vcf files from different callers which are \n
-    given as options. Please notic that the name of each vcf file has to satisfy this pattern.
-    '*(nameofcaller).vcf.gz , e.g(*vardict.vcf.gz)'.
-    The name of out file and the durectory where all vcf are located it are required to be passed as options.
-    In the output file, All unique varinat lines and  contig,INFO heaer lines are saved. 
-       
-''' 
+
+
+def main():
+    """    
+    This script merges the vcf files from different callers which are
+    given as options. Please notice that the name of each vcf file has to satisfy this pattern:
+    '*nameofcaller.vcf.gz (e.g. *vardict.vcf.gz)'.
+    The name of out file and the directory where all vcf are located in are required to be passed as other options.
+    In the output file, All unique varinat lines and contig,INFO,ALT header lines are written.        
+    """ 
+    print(main.__doc__)
+    Parser = OptionParser()
+    Parser.add_option("--callers", action='store', dest='callers',help="A list of variant callers to be included.")
+    Parser.add_option("--output", action='store', dest='output_name', help = "Name of outfile")
+    Parser.add_option("--dir", action='store', dest='analysis_dirPath', help= "Tha PATH of the directory where vcf files locate.\nAccepted pattern for vcf files: *nameofcaller.vcf.gz (e.g. *vardict.vcf.gz)")
+    
+
+    #(options, args) = Parser.parse_args()
+    options=  Parser.parse_args()[0]
+
+    callers= options.callers
+    callers = callers.split(',')
+    analysis_dirPath = options.analysis_dirPath
+    fout_name = options.output_name
+
+    all_vcfs =[]
+
+    for name in callers:
+        pattern = '*' + name + '.vcf.gz'
+        #globals()[name] = fnmatch.filter(os.listdir(analysis_dirPath), pattern)
+        files = fnmatch.filter(os.listdir(analysis_dirPath), pattern)
+        all_vcfs = all_vcfs + files
+    merge_vcfs(all_vcfs=all_vcfs,analysis_dirPath=analysis_dirPath,fout_name=fout_name)
+
+    #analysis_dirPath = '/fs1/results/myeloid/vcf/'
+    #freebayes = fnmatch.filter(os.listdir(analysis_dirPath), '*freebayes.vcf.gz')
+    #vardict = fnmatch.filter(os.listdir(analysis_dirPath), '*vardict.vcf.gz')
+    #tnscope = fnmatch.filter(os.listdir(analysis_dirPath), '*tnscope.vcf.gz')
+    #all_vcfs= freebayes +  vardict +  tnscope
+
+
+if __name__ == '__main__':
+    main()
